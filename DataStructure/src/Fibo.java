750839920a3d0b5d5931dd38744a7f994846dc7f
Ken Nguyen <khangnguyen291@gmail.com>
2022-10-31 18:28:04 +1100

    Fibonacci homework: memoization

--- DataStructure/src/Fibo.java
+++ DataStructure/src/Fibo.java
@@ -1,20 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Fibo {
+
+    static int[] memo;
     public static void main(String[] args) {
-        System.out.println(fiboOf(45));
+        int n = 20;
+        memo = new int[n + 1];
+        System.out.print(fibonacciMemo(n));
     }
-    public static int fiboOf(int number){
+
+    public static int fibonacciMemo(int n){
+
+        //memo[n] != 0 nghia la da dc tinh roi, cu su dung lai, ko can tinh nua
+        if (memo[n] != 0) return memo[n];
+
+        //base case
+        if (n == 0 || n ==1){
+            return n;
+        }
+
+        int answer = fibonacciMemo(n-1) + fibonacciMemo(n-2);
+        //moi lan tinh dc so fibonacci nao thi luu lai vao memo de su dung vao nhung lan sau
+        memo[n] = answer;
+        return answer;
+        //TIME COMPLEXITY O(n)
+    }
+
+    public static int fibonacciIteration(int number){
         int fibo0 = 0;
         int fibo1 = 1;
 
         int fiboN = 0;
 
-       for (int i =0; i < number - 1; i++){
-           fiboN = fibo0 + fibo1;
-           fibo0 = fibo1;
-           fibo1 = fiboN;
-       };
-
-       return fiboN;
-       // TIME COMPLEXITY O(n-1)
+        for (int i =0; i < number - 1; i++){
+            fiboN = fibo0 + fibo1;
+            fibo0 = fibo1;
+            fibo1 = fiboN;
+        };
+        return fiboN;
     }
+
+
 }
